20a05824479574cd919dc16d58bd37a8e4bf4c08
elvalitowa <elya.valitowa@yandex.ru>
2016-09-07 13:49:51 +0200

    Created by Coding Ground Client

---
+++ CM2016/lab_01_01.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+void main () {
+  int x=23;
+  int y=24;
+  printf("x+y=%d\n",x+y);
+  printf("x-y=%d\n",x-y);
+  printf("x*y=%d\n",x*y);
+  printf("x%%y=%d\n",x%y);
+  printf("x/y=%d\n",x/y);
+  double a=12.7,
+         b=4.5;
+  printf("a/b=%lf\n",a/b);
+  printf("x+a=%lf\n",x+a);
+  char c1=100,
+       c2=200;
+ printf("c1+c2=%d\n",c1+c2);
+ int i=15;
+ printf("i=%d\n",i);
+ i=i+1;
+ printf("i=i+1:%d\n",i);
+ i++;
+ printf("i++:%d\n",i);
+ i+=1;
+ printf("i+=:%d\n",i);
+ 
+ i=i-1;
+ printf("i=i-1:%d\n",i);
+ 
+ i--;
+ printf("i--:%d\n",i);
+ 
+ i-=1;
+ printf("i-=:%d\n",i);
+ 
+ 
+ 
+  
+}
